9bc0338418ece7a43f2386b74f203e329abff887
hongge <1361255790.@qq.com>
2018-07-03 15:26:37 +0800

    init

---
+++ src/test/java/com/hc/read/testXML.java
@@ -0,0 +1,14 @@
+package com.hc.read;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testXML {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
